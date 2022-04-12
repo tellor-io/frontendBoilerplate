0a2ec357304786fbff43f28f996611dd583ea09b
Joshua Samaniego <joshuadsamaniego@gmail.com>
2022-04-12 15:59:04 -0700

    adds light/dark mode switcher

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useContext } from 'react'
 //Components
-import Nav from "./components/frontendBoilerplate/Nav";
-import Footer from "./components/frontendBoilerplate/Footer";
+import Nav from './components/frontendBoilerplate/Nav'
+import Footer from './components/frontendBoilerplate/Footer'
 //Styles
-import './App.css';
+import './App.css'
+//Context
+import { ModeContext } from './contexts/Mode'
 
 function App() {
+  //Context
+  const mode = useContext(ModeContext)
   return (
-    <div className="App">
+    <div className={mode && mode.mode === 'dark' ? 'App' : 'AppLight'}>
       <Nav />
-      <div className="HeroContainer">
+      <div
+        className={
+          mode && mode.mode === 'dark' ? 'HeroContainer' : 'HeroContainerLight'
+        }
+      >
         <h1>Your App Goes Here</h1>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
